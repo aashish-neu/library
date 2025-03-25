3513b954774f68c763074582505b724e1cfe8caa
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-03-25 17:44:30 +0545

    Update button and book cards styles

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// selectors\
 const bookWrap = document.querySelector(".book-wrap");
 
 let myLibrary = [];
@@ -19,41 +18,50 @@ Book.prototype.setBookId = function () {
   return crypto.randomUUID();
 };
 
-function addBookToLibrary(title, author, noOfPages, hasRead) {
+function addBookToLibrary() {
+  title = "The Lord of the Rings";
+  author = "J.R.R. Tolkein";
+  noOfPages = 1137;
+  hasRead = false;
   let newBook = new Book(title, author, noOfPages, hasRead);
   newBook.bookId = newBook.setBookId();
   myLibrary.push(newBook);
 
-  // creates html for new book
-  const singleBook = document.createElement("div");
-  singleBook.classList.add("book-card");
-  bookWrap.appendChild(singleBook);
-
-  const bookTitle = document.createElement("h3");
-  bookTitle.classList.add("book-title");
-  bookTitle.innerText = prompt("What is book title?");
-  singleBook.appendChild(bookTitle);
-
-  const bookAuthor = document.createElement("h4");
-  bookAuthor.classList.add("book-author");
-  bookAuthor.innerText = prompt("Who is book author?");
-  singleBook.appendChild(bookAuthor);
-
-  const bookPages = document.createElement("span");
-  bookPages.classList.add("book-pages");
-  bookPages.innerText = prompt("How many pages are there?");
-  singleBook.appendChild(bookPages);
-
-  const bookReadStatus = document.createElement("button");
-  bookReadStatus.classList.add("book-read-status-btn");
-  bookReadStatus.innerText = prompt("Have you read the book?");
-  singleBook.appendChild(bookReadStatus);
-
-  //   call displayLibrary()
+  displayLibrary();
 }
 
+
 function displayLibrary() {
-  for (let i = 0; i < myLibrary.length; i++) {}
+  // remove previously displayed books
+
+  bookWrap.replaceChildren();
+
+  // display updated books
+  for (let loopingBook of myLibrary) {
+    const singleBook = document.createElement("div");
+    singleBook.classList.add("book-card");
+    bookWrap.appendChild(singleBook);
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.classList.add("book-title");
+    bookTitle.innerText = loopingBook.title;
+    singleBook.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("h4");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.innerText = loopingBook.author;
+    singleBook.appendChild(bookAuthor);
+
+    const bookPages = document.createElement("span");
+    bookPages.classList.add("book-pages");
+    bookPages.innerText = loopingBook.noOfPages;
+    singleBook.appendChild(bookPages);
+
+    const bookReadStatus = document.createElement("button");
+    bookReadStatus.classList.add("book-read-status-btn");
+    bookReadStatus.innerText = loopingBook.hasRead;
+    singleBook.appendChild(bookReadStatus);
+  }
 }
 
 const addBookBtn = document.getElementById("add-book-btn");
