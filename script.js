1e72ac9f1f8189204d9b539f2e7ec0088d83e6e8
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-03-25 15:40:36 +0545

    Add book styling and create book dom scripts

--- script.js
+++ script.js
@@ -24,14 +24,30 @@ function addBookToLibrary(title, author, noOfPages, hasRead) {
   newBook.bookId = newBook.setBookId();
   myLibrary.push(newBook);
 
-  // creates dom for new book
+  // creates html for new book
   const singleBook = document.createElement("div");
   singleBook.classList.add("book-card");
-  singleBook.textContent = "Testing Dom";
   bookWrap.appendChild(singleBook);
 
+  const bookTitle = document.createElement("h3");
+  bookTitle.classList.add("book-title");
+  bookTitle.innerText = prompt("What is book title?");
+  singleBook.appendChild(bookTitle);
 
+  const bookAuthor = document.createElement("h4");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.innerText = prompt("Who is book author?");
+  singleBook.appendChild(bookAuthor);
 
+  const bookPages = document.createElement("span");
+  bookPages.classList.add("book-pages");
+  bookPages.innerText = prompt("How many pages are there?");
+  singleBook.appendChild(bookPages);
+
+  const bookReadStatus = document.createElement("button");
+  bookReadStatus.classList.add("book-read-status-btn");
+  bookReadStatus.innerText = prompt("Have you read the book?");
+  singleBook.appendChild(bookReadStatus);
 
   //   call displayLibrary()
 }
@@ -40,5 +56,5 @@ function displayLibrary() {
   for (let i = 0; i < myLibrary.length; i++) {}
 }
 
-// const addBookBtn = document.getElementById("add-book-btn");
-// addBookBtn.addEventListener("click", addBookToLibrary);
+const addBookBtn = document.getElementById("add-book-btn");
+addBookBtn.addEventListener("click", addBookToLibrary);
