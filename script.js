1e50ccb4387bef011a57f2c48339fffa1e38243a
Aashish N <165367795+aashish-neu@users.noreply.github.com>
2025-03-25 14:47:18 +0545

    Add displayLibrary and addBookToLibrary functions

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
+// selectors\
+const bookWrap = document.querySelector(".book-wrap");
+
 let myLibrary = [];
 
 function Book(title, author, noOfPages, hasRead) {
-    this.title = title,
-    this.author = author,
-    this.noOfPages = noOfPages,
-    this.hasRead = hasRead,
-
-    this.info = function(){
-        return(`${this.title} by ${this.author}, ${noOfPages} pages, ${hasRead ? 'read' : 'not read yet'}`);
-    }
+  (this.title = title),
+    (this.author = author),
+    (this.noOfPages = noOfPages),
+    (this.hasRead = hasRead),
+    (this.info = function () {
+      return `${this.title} by ${this.author}, ${noOfPages} pages, ${
+        hasRead ? "read" : "not read yet"
+      }`;
+    });
 }
 
-Book.prototype.setBookId = function() {
-    return crypto.randomUUID();
-} 
+Book.prototype.setBookId = function () {
+  return crypto.randomUUID();
+};
+
+function addBookToLibrary(title, author, noOfPages, hasRead) {
+  let newBook = new Book(title, author, noOfPages, hasRead);
+  newBook.bookId = newBook.setBookId();
+  myLibrary.push(newBook);
+
+  // creates dom for new book
+  const singleBook = document.createElement("div");
+  singleBook.classList.add("book-card");
+  singleBook.textContent = "Testing Dom";
+  bookWrap.appendChild(singleBook);
 
 
 
 
-function addBookToLibrary() {
-
+  //   call displayLibrary()
 }
 
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {}
+}
 
-// const book1 = new Book ('The Hobbit', "J.R.R. Tolkein", 295, false);
-// console.log(book1.info())
+// const addBookBtn = document.getElementById("add-book-btn");
+// addBookBtn.addEventListener("click", addBookToLibrary);
